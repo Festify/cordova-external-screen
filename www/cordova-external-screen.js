cd823e6a0ccc81a2e4742b576502fd1c18534b36
Leo Bernard <admin@leolabs.org>
2017-01-21 03:04:53 +0100

    :sparkles: Add JS connector

--- www/cordova-external-screen.js
+++ www/cordova-external-screen.js
@@ -1,21 +1,14 @@
-exports["airPlayScreen"] = {
-setupScreenConnectionNotificationHandlers: function (success, fail) {
-    return PhoneGap.exec(success, fail, "AirPlayScreen", "setupScreenConnectionNotificationHandlers", []);
-},
-    
-loadHTMLResource: function (url, success, fail) {
-    return PhoneGap.exec(success, fail, "AirPlayScreen", "loadHTMLResource", [url]);
-},
-    
-loadHTML: function (url, success, fail) {
-    return PhoneGap.exec(success, fail, "AirPlayScreen", "loadHTML", [url]);
-},
-    
-invokeJavaScript: function (scriptString, success, fail) {
-    return PhoneGap.exec(success, fail, "AirPlayScreen", "invokeJavaScript", [scriptString]);
-},
-    
-checkExternalScreenAvailable: function (success, fail) {
-    return PhoneGap.exec(success, fail, "AirPlayScreen", "checkExternalScreenAvailable", []);
+require("es6-promise/auto");
+
+const EventEmitter = reqire("./lib/EventEmitter.js");
+const exec = require("./lib/execPromise.js");
+
+class ExternalScreen extends EventEmitter {
+    loadUrl(url) {
+        return exec("loadURL", [url]);
+    }
 }
-};
+
+exports.createScreenListener = function() {
+    return (new ExternalScreen()).registerEvents();
+}
