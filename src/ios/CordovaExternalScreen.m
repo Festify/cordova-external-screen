18f2ce60a39fffcbce39685147f7ee996bae630d
Moritz Gunz <moritz.gunz@gmail.com>
2017-01-28 11:23:25 +0100

    :art: Improve formatting in CordovaExternalScreen.m

--- src/ios/CordovaExternalScreen.m
+++ src/ios/CordovaExternalScreen.m
@@ -2,61 +2,19 @@
 
 @implementation CordovaExternalScreen
 
--(void)pluginInitialize {
+- (void)pluginInitialize {
     __weak id <CDVCommandDelegate> _commandDelegate = self.commandDelegate;
     self.screenNotificationDelegate = [ScreenNotificationDelegate eventEmitterWithCommandDelegate: _commandDelegate];
 }
 
--(void)registerEventsListener:(CDVInvokedUrlCommand*)command {
-    NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
-
-    [self.screenNotificationDelegate setCallbackId:command.callbackId];
-
-    [center addObserver:self.screenNotificationDelegate selector:@selector(handleScreenConnectNotification:)
-                   name:UIScreenDidConnectNotification object:nil];
-    [center addObserver:self.screenNotificationDelegate selector:@selector(handleScreenDisconnectNotification:)
-                   name:UIScreenDidDisconnectNotification object:nil];
-
-    [center addObserver:self selector:@selector(handleScreenDisconnectNotification:)
-                   name:UIScreenDidDisconnectNotification object:nil];
-
-    CDVPluginResult *result = [CDVPluginResult
-                               resultWithStatus: CDVCommandStatus_OK
-                               messageAsString: nil];
-    [result setKeepCallbackAsBool:YES];
-
-    [self.commandDelegate sendPluginResult: result callbackId: command.callbackId];
-}
-
--(void)loadURL:(CDVInvokedUrlCommand*)command {
-    if (![self hasExternalScreen]) {
-        CDVPluginResult *result = [CDVPluginResult
-                                   resultWithStatus: CDVCommandStatus_ERROR
-                                   messageAsString: @"No external screens ¯\\_(ツ)_/¯"];
-
-        [self.commandDelegate sendPluginResult: result callbackId: command.callbackId];
-        return;
-    }
-
-    NSURL* url = [NSURL URLWithString: [command.arguments objectAtIndex: 0]];
-    NSURLRequest* request = [NSURLRequest requestWithURL: url];
-    [[self getWebView] loadRequest: request];
-
-    CDVPluginResult *result = [CDVPluginResult
-                               resultWithStatus: CDVCommandStatus_OK];
-
-    [self.commandDelegate sendPluginResult: result callbackId: command.callbackId];
-}
-
--(WKWebView*)getWebView {
+- (WKWebView*)getWebView {
     if (!self.externalWebView) {
-        UIScreen* externalScreen = [[UIScreen screens] objectAtIndex:1];
-
+        UIScreen* externalScreen = [[UIScreen screens] objectAtIndex: 1];
         CGRect screenBounds = externalScreen.bounds;
 
-        self.externalWebView = [[WKWebView alloc] initWithFrame:screenBounds configuration:
-                        [[WKWebViewConfiguration alloc] init]];
-        self.externalWindow = [[UIWindow alloc] initWithFrame:screenBounds];
+        self.externalWebView = [[WKWebView alloc] initWithFrame: screenBounds
+                                                  configuration: [[WKWebViewConfiguration alloc] init]];
+        self.externalWindow = [[UIWindow alloc] initWithFrame: screenBounds];
 
         self.externalWindow.screen = externalScreen;
         self.externalWindow.clipsToBounds = YES;
@@ -68,19 +26,63 @@
     return self.externalWebView;
 }
 
--(bool) hasExternalScreen {
+- (bool) hasExternalScreen {
     return ([[UIScreen screens] count] > 1);
 }
 
--(void) hasExternalScreen:(CDVInvokedUrlCommand*)command {
-    CDVPluginResult *result = [CDVPluginResult
-                               resultWithStatus: CDVCommandStatus_OK
-                               messageAsBool: [self hasExternalScreen]];
+- (void) hasExternalScreen:(CDVInvokedUrlCommand*)command {
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK
+                                                  messageAsBool: [self hasExternalScreen]];
 
-    [self.commandDelegate sendPluginResult: result callbackId: command.callbackId];
+    [self.commandDelegate sendPluginResult: result
+                                callbackId: command.callbackId];
 }
 
--(void) handleScreenDisconnectNotification:(NSNotification*)aNotification {
+- (void)loadURL:(CDVInvokedUrlCommand*)command {
+    if (![self hasExternalScreen]) {
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR
+                                                    messageAsString: @"No external screens ¯\\_(ツ)_/¯"];
+
+        [self.commandDelegate sendPluginResult: result callbackId: command.callbackId];
+        return;
+    }
+
+    NSURL* url = [NSURL URLWithString: [command.arguments objectAtIndex: 0]];
+    NSURLRequest* request = [NSURLRequest requestWithURL: url];
+    [[self getWebView] loadRequest: request];
+
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK];
+
+    [self.commandDelegate sendPluginResult: result
+                                callbackId: command.callbackId];
+}
+
+- (void)registerEventsListener:(CDVInvokedUrlCommand*)command {
+    NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+
+    [self.screenNotificationDelegate setCallbackId: command.callbackId];
+
+    [center addObserver: self.screenNotificationDelegate
+               selector: @selector(handleScreenConnectNotification:)
+                   name: UIScreenDidConnectNotification
+                 object: nil];
+    [center addObserver: self.screenNotificationDelegate
+               selector: @selector(handleScreenDisconnectNotification:)
+                   name: UIScreenDidDisconnectNotification object:nil];
+
+    [center addObserver: self
+               selector: @selector(handleScreenDisconnectNotification:)
+                   name: UIScreenDidDisconnectNotification object:nil];
+
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK
+                                                messageAsString: nil];
+    [result setKeepCallbackAsBool: YES];
+
+    [self.commandDelegate sendPluginResult: result
+                                callbackId: command.callbackId];
+}
+
+- (void) handleScreenDisconnectNotification:(NSNotification*)aNotification {
 
 }
 
